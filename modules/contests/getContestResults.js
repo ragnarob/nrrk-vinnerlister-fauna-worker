cffb8af15dc30e437eb21ae49ddd1bbecb0fd8fd
Ragnar Bråstein <ragnarob@protonmail.com>
2022-02-27 23:39:25 +0100

    Return empty arrays instead of throwing error when no data found

--- modules/contests/getContestResults.js
+++ modules/contests/getContestResults.js
@@ -1,35 +1,44 @@
 import {
   Collection, Select, Get, Index, Let, Lambda, Map, Match, Paginate, Ref, Var,
 } from 'faunadb';
+import { getFaunaError } from '../utils';
 
 export default async function getContestResults(faunaClient, contestId) {
-  const result = await faunaClient.query(
-    Map(
-      Paginate(
-        Match(
-          Index('results_by_contest'),
-          Ref(Collection('Contests'), contestId),
+  try {
+    const result = await faunaClient.query(
+      Map(
+        Paginate(
+          Match(
+            Index('results_by_contest'),
+            Ref(Collection('Contests'), contestId),
+          ),
+        ),
+        Lambda(
+          ['resultRef', 'dogRef', 'contestRef'],
+          Let(
+            {
+              resultDoc: Get(Var('resultRef')),
+              dogDoc: Get(Var('dogRef')),
+            },
+            {
+              resultId: Select(['ref', 'id'], Var('resultDoc')),
+              result: Select(['data', 'result'], Var('resultDoc')),
+              critiqueLink: Select(['data', 'critiqueLink'], Var('resultDoc')),
+
+              dogId: Select(['ref', 'id'], Var('dogDoc')),
+              dogName: Select(['data', 'name'], Var('dogDoc')),
+            },
+          ),
         ),
       ),
-      Lambda(
-        ['resultRef', 'dogRef', 'contestRef'],
-        Let(
-          {
-            resultDoc: Get(Var('resultRef')),
-            dogDoc: Get(Var('dogRef')),
-          },
-          {
-            resultId: Select(['ref', 'id'], Var('resultDoc')),
-            result: Select(['data', 'result'], Var('resultDoc')),
-            critiqueLink: Select(['data', 'critiqueLink'], Var('resultDoc')),
+    );
 
-            dogId: Select(['ref', 'id'], Var('dogDoc')),
-            dogName: Select(['data', 'name'], Var('dogDoc')),
-          },
-        ),
-      ),
-    ),
-  );
-
-  return result.data;
+    return result.data;
+  } catch (err) {
+    const faunaError = getFaunaError(err);
+    if (faunaError.status === 404) {
+      return [];
+    }
+    throw err;
+  }
 }
--- modules/index-by-year/resultsByYear.js
+++ modules/index-by-year/resultsByYear.js
@@ -1,40 +1,49 @@
 import {
   Get, Index, Lambda, Map, Match, Paginate, Var, Join, Let, Select,
 } from 'faunadb';
+import { getFaunaError } from '../utils';
 
 export default async function getResultsByYear(faunaClient, year) {
-  const result = await faunaClient.query(
-    Map(
-      Paginate(
-        Join(
-          Match(
-            Index('contest_by_year'),
-            year,
+  try {
+    const result = await faunaClient.query(
+      Map(
+        Paginate(
+          Join(
+            Match(
+              Index('contest_by_year'),
+              year,
+            ),
+            Lambda(
+              ['date', 'contestRef'],
+              Match(Index('results_by_contest'), Var('contestRef')), // results_by_contest from 'contests' folder
+            ),
           ),
-          Lambda(
-            ['date', 'contestRef'],
-            Match(Index('results_by_contest'), Var('contestRef')), // results_by_contest from 'contests' folder
+        ),
+        Lambda(
+          ['resultRef', 'dogRef', 'contestRef'],
+          Let(
+            {
+              resultDoc: Get(Var('resultRef')),
+              dogDoc: Get(Var('dogRef')),
+              contestDoc: Get(Var('contestRef')),
+            },
+            {
+              result: Select(['data', 'result'], Var('resultDoc')),
+              dogName: Select(['data', 'name'], Var('dogDoc')),
+              dogId: Select(['ref', 'id'], Var('dogDoc')),
+              numberOfDogs: Select(['data', 'numberOfDogs'], Var('contestDoc')),
+            },
           ),
         ),
       ),
-      Lambda(
-        ['resultRef', 'dogRef', 'contestRef'],
-        Let(
-          {
-            resultDoc: Get(Var('resultRef')),
-            dogDoc: Get(Var('dogRef')),
-            contestDoc: Get(Var('contestRef')),
-          },
-          {
-            result: Select(['data', 'result'], Var('resultDoc')),
-            dogName: Select(['data', 'name'], Var('dogDoc')),
-            dogId: Select(['ref', 'id'], Var('dogDoc')),
-            numberOfDogs: Select(['data', 'numberOfDogs'], Var('contestDoc')),
-          },
-        ),
-      ),
-    ),
-  );
+    );
 
-  return result.data;
+    return result.data;
+  } catch (err) {
+    const faunaError = getFaunaError(err);
+    if (faunaError.status === 404) {
+      return [];
+    }
+    throw err;
+  }
 }
